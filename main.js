2ab06e84a2784e643b59973243b6c36276bad609
Pulga <juansie.96@gmail.com>
2020-03-16 18:24:01 -0300

    Finalizadas funcionalidades del boton back-to-top. Avances del responsive con las fuentes y los menus.

--- main.js
+++ main.js
@@ -19,6 +19,16 @@ function normalImg(image) {
     image.style.width = "60%";
 }
 
-$('#id').click(function() {
-    console.log('scrolling')
+// ===== Scroll to Top ==== 
+$(window).scroll(function () {
+    if ($(this).scrollTop() >= 50) { // If page is scrolled more than 50px
+        $('#top').fadeIn("fast"); // Fade in the arrow
+    } else {
+        $('#top').fadeOut("fast"); // Else fade out the arrow
+    }
+});
+$('#top').click(function () { // When arrow is clicked
+    $('body,html').animate({
+        scrollTop: 0 // Scroll to top of body
+    }, 500);
 });
